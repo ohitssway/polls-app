89d44649dc52bfc43ef1b750d7b658b27aa92a78
Saimun <saimun.shahee@gmail.com>
2016-12-17 21:45:21 -0500

    A polls app created using Django and Bootstrap

---
+++ mysite/polls/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name="index"),
+    #localhost:8000/polls
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name="detail"),
+    #localhost:8000/polls/1
+    url(r'^(?P<question_id>[0-9]+)/results$', views.results, name="results"),
+    #localhost:8000/polls/1/results
+    url(r'^(?P<question_id>[0-9]+)/vote$', views.vote, name="vote"),
+    #localhost:8000/polls/1/vote
+]
